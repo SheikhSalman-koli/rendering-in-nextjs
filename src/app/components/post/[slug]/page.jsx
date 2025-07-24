8a56aefe2adb2f34434491e7733229f1934972e8
SheikhSalman <assalmanmuhammad@gmail.com>
2025-07-24 07:24:08 +0600

    metadata

--- src/app/components/post/[slug]/page.jsx
+++ src/app/components/post/[slug]/page.jsx
@@ -6,13 +6,29 @@ export  const singlePost = async(postId)=>{
     return data
 }
 
+export async function generateMetadata({ params }) {
+  // read route params
+  const id =await params.slug
+ 
+  // fetch data
+  const post = await singlePost(id)
+
+  return {
+    title: post.title,
+    description: post.body
+  }
+}
+ 
+
 export default async function DetailsPage({params}) {
+
     const id =await params.slug
     const post = await singlePost(id)
+
   return (
     <div>
         <p>DetailsPage</p>
-        <h1>{post.title}</h1>
+        <h1 className='globally-accessable-css'>{post.title}</h1>
         <p>{post.body}</p>
     </div>
   )
---
+++ src/app/meals/[id]/page.jsx
@@ -0,0 +1,48 @@
+
+import style from '../../components/mealhead.module.css'
+
+    const fetchSingleMeals = async (id) => {
+        try {
+            const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+            const data = await res.json()
+            return data.meals || []
+        } catch (error) {
+            console.log(error);
+            return []
+        }
+    }
+
+
+    export async function generateMetadata({ params }) {
+      // read route params
+      const id =await params?.id
+     
+      // fetch data
+      const [meal] = await fetchSingleMeals(id)
+    
+      return {
+        title: meal.strMeal,
+        description: meal.strArea
+      }
+    }
+
+
+
+export default async function page({ params }) {
+    const id = await params?.id
+
+    const [meal] = await fetchSingleMeals(id)
+
+    return (
+        <div className='p-8 space-y-6'>
+            <div
+                className='border-2 rounded-2xl space-y-2.5 p-3'
+            >
+                <h1 className={style.mealheading}>{meal.strMeal}</h1>
+                <p className='text-orange-500'>{meal.strArea}</p>
+                <p className='text-blue-500'>{meal.strInstructions}</p>
+            </div>
+
+        </div>
+    )
+}
